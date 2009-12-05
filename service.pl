b66c3231513582cb27437c87ecc6a1db3f2f1e83
boaz <boaz@boaz-laptop>
2009-12-05 20:06:39 +0200

    completed writing

--- service.pl
+++ service.pl
@@ -1,5 +1,5 @@
 % Service utilities for the Cedalion commands
-:- [cedalion].
+:- [cedalion, termio].
 :- op(1200, xfy, '~>').
 :- op(100, xfx, ':').
 
@@ -78,7 +78,7 @@ termLocalToGlobal(Term, Namespace, NsList, Global) :-
 	Term =.. [Func | Args],
 	if(dontConvertFunc(Func),
 		GFunc = Func,
-		concat_atom([Namespace, ':', Func], GFunc)),
+		concat_atom([Namespace, '#', Func], GFunc)),
 	localToGlobalList(Args, NsList, GArgs),
 	Global =.. [GFunc | GArgs].
 
@@ -135,7 +135,6 @@ convertVarNames([Name=Var | RestIn], ['cedalion-services:varName'(Var::_, Name) 
 % Write a cedalion file
 writeFile(FileName, 'cedalion-services:fileContent'(Terms, NsList)) :-
 	open(FileName, write, Stream),
-	writeImports(Stream, NsList),
 	writeToStream(Stream, Terms, NsList).
 
 writeToStream(Stream, [], _) :-
@@ -143,14 +142,15 @@ writeToStream(Stream, [], _) :-
 
 writeToStream(Stream, ['cedalion-services:statement'(GTerm, VarNames) | OtherTerms], NsList) :-
 	globalToLocal(GTerm, NsList, Term),
-	writeTerm(Stream, Term, VarNames).
+	writeTerm(Stream, Term, VarNames),
+	writeToStream(Stream, OtherTerms, NsList).
 
 globalToLocal(GTerm, NsList, Term) :-
 	if(nonCompoundTerm(GTerm),
 		Term = GTerm,
 		(
 			GTerm =.. [GFunc | GArgs],
-			globalToLocalList(GArgs, Args),
+			globalToLocalList(GArgs, NsList, Args),
 			if(splitNamespace(GFunc, Namespace, Func),
 				(
 					LTerm =.. [Func | Args],
@@ -162,14 +162,14 @@ globalToLocal(GTerm, NsList, Term) :-
 				Term =.. [GFunc | Args])
 		)).
 
-globalToLocalList([], []).
-globalToLocalList([GArg | GArgs], [Arg | Args]) :-
-	globalToLocal(GArg, Arg),
-	globalToLocalList(GArgs, Args).
+globalToLocalList([], _, []).
+globalToLocalList([GArg | GArgs], NsList, [Arg | Args]) :-
+	globalToLocal(GArg, NsList, Arg),
+	globalToLocalList(GArgs, NsList, Args).
 
 splitNamespace(GlobalName, NsAlias, LocalName) :-
 	atom_chars(GlobalName, Chars),
-	append(Pre, [':' | Post], Chars),
+	append(Pre, ['#' | Post], Chars),
 	atom_chars(NsAlias, Pre),
 	atom_chars(LocalName, Post).
 
@@ -183,6 +183,6 @@ writeImports(_, []).
 writeImports(Stream, [Alias = Namespace | NsList]) :-
 	if(Alias = default,
 		true,
-		writeTerm(Stream, import(s(Alias), s(Namespace), [])),
+		writeTerm(Stream, import(s(Alias), s(Namespace)), [])),
 	writeImports(Stream, NsList).
 
---
+++ termio.pl
@@ -0,0 +1,76 @@
+% Write a term to a stream from a term(Term, VarNames) tupple
+writeTerm(Stream, Term, VarNames) :-
+	convertTermToWritable(Term, VarNames, WTerm),
+	write_term(Stream, WTerm, [quoted(false)]),
+	write(Stream, '.\n').
+
+% Convert a term(Term, VarNames) tupple into a term to be written without quotes:
+% Variables are converted to contain their to their names, atoms are quoted if needed
+convertTermToWritable(Var, VarNames, Name) :-
+	var(Var),
+	findVarName(Var, VarNames, Name),
+	!.
+convertTermToWritable(Var, _, '_') :-
+	var(Var),
+	!.
+convertTermToWritable(Num, _, Num) :-
+	number(Num),
+	!.
+convertTermToWritable(Term, VarNames, WTerm) :-
+	Term =.. [Atom | Args],
+	quoteAtomIfNeeded(Atom, QAtom),
+	convertTermsToWritable(Args, VarNames, WArgs),
+	WTerm =.. [QAtom | WArgs].
+	
+convertTermsToWritable([], _, []).
+convertTermsToWritable([Term | Terms], VarNames, [WTerm | WTerms]) :-
+	convertTermToWritable(Term, VarNames, WTerm),
+	convertTermsToWritable(Terms, VarNames, WTerms).
+
+% Find the name of a varialbe in a list of Name=Var
+findVarName(Var, ['cedalion-services:varName'(Var1::_, Name) | _], Name) :-
+	Var == Var1.
+
+findVarName(Var, [_| VarNames], Name) :-
+	findVarName(Var, VarNames, Name).
+
+% Check if an atom needs to be quoted
+simpleAtom(Atom) :-
+	atom_codes(Atom, [First | Rest]),
+	First >= 97,
+	First =< 122,
+	forall(member(Code, Rest), identifierCode(Code)).
+	
+% Codes that may appear in an unquoted atom
+identifierCode(Code) :-
+	Code >= 97,
+	Code =< 122.
+identifierCode(Code) :-
+	Code >= 65,
+	Code =< 90.
+identifierCode(95).
+
+% Quote an atom
+quote(Atom, QAtom) :-
+	atom_chars(Atom, Chars),
+	quoteList(Chars, QChars),
+	atom_chars(QAtom, ['\'' | QChars]).
+
+% Quote an atom given as a list of characters
+quoteList([], ['\'']).
+quoteList(['\'' | Rest], ['\\', '\'' | QRest]) :-
+	!,
+	quoteList(Rest, QRest).
+
+quoteList([Char | Rest], [Char | QRest]) :-
+	quoteList(Rest, QRest).
+
+% Quote if needed
+quoteAtomIfNeeded(Atom, Atom) :-
+	simpleAtom(Atom),
+	!.
+quoteAtomIfNeeded(Atom, Atom) :-
+	current_op(_, _, Atom),
+	!.
+quoteAtomIfNeeded(Atom, QAtom) :-
+	quote(Atom, QAtom).
